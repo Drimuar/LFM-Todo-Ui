059d6ec215adfc376b76cb128a5501e19746dc38
Drimuar <Drimuar@gmail.com>
2022-04-23 13:42:19 +0700

    refactor and style

---
+++ js/controler.js
@@ -0,0 +1,5 @@
+let mainFrame = document.querySelector('.main-frame');
+export let addForm = mainFrame.getElementsByClassName('add-form');
+export let removeButtons = mainFrame.getElementsByClassName('remove-button');
+export let checkboxes = mainFrame.getElementsByClassName('checkbox');
+export let container = mainFrame.getElementsByClassName('container');
--- js/view.js
+++ js/view.js
@@ -1,5 +1,61 @@
-let mainFrame = document.querySelector('.main-frame');
-export let addForm = mainFrame.getElementsByClassName('add-form');
-export let removeButtons = mainFrame.getElementsByClassName('remove-button');
-export let checkboxes = mainFrame.getElementsByClassName('checkbox');
-export let container = mainFrame.getElementsByClassName('container');
+import { removeButtons, checkboxes, addForm, container } from "./controler.js";
+
+const priorityList = {
+	high: 0,
+	low: 1,
+}
+
+const tasks = [];
+
+for (let form of addForm) {
+	form.addEventListener("submit", createTask);
+}
+
+function Task(text, priority) {
+	this.text = text;
+	this.priority = priority;
+}
+
+function addEventOnButtons() {
+	for (let button of removeButtons) {
+		button.addEventListener("click", removeTask)
+	}
+	for (let checkbox of checkboxes) {
+		checkbox.addEventListener("click", markTask)
+	}
+}
+addEventOnButtons();
+
+function createTask(event) {
+	event.preventDefault();
+	let textValue = this.text.value;
+	if (!textValue) {
+		return;
+	}
+	let priority = this.getAttribute('name');
+	tasks.push(new Task(textValue, priority));
+	console.log(tasks);
+	let currentContainer = container[priorityList[priority]];
+	currentContainer.insertAdjacentHTML("beforeend", `<li class="task"><label class="checkbox-wrap"><input class="checkbox" type="checkbox"></label><p class="text">${textValue}</p><button class="remove-button"></button></li>`);
+	addEventOnButtons();
+}
+
+function removeTask() {
+	let div = this.closest('.task');
+	if (!!div) {
+		div.remove();
+	}
+}
+
+function markTask() {
+	let div = this.closest('.task');
+	if (!!div) {
+		if (this.checked) {
+			div.classList.add('task-done');
+		}
+		else {
+			div.classList.remove('task-done');
+		}
+	}
+}
+
